11dc6c3efd0107fe6c63db1235ab78eff53402cb
Touha Zohair <touha98@gmail.com>
2025-02-16 18:32:10 +0600

    remove db module

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,4 +1,4 @@
-import {} from 'database';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
